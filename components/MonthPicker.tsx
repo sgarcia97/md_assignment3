0195d8058f1a813a522c361848e678d4d8e9fe31
glconde <georgelouie.conde@edu.sait.ca>
2025-03-05 21:44:34 -0700

    new changes

--- components/MonthPicker.tsx
+++ components/MonthPicker.tsx
@@ -1,63 +1,84 @@
-import { Text, View, StyleSheet, TouchableHighlight, TouchableOpacity} from "react-native" 
-import { useState } from "react"
-import Months from './Months'
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
+import Months from "./Months";
 
 type PickerType = {
-    setMonth: (value:string) => void;
-    month:string;
-}
-const MonthPicker = ({setMonth, month}:PickerType) => {
-    const [dropdown, setDropdown] = useState(false)
-    const [selected, setSelected] = useState(false)
-    return(
-        <>
-        <TouchableHighlight style={{borderRadius:5}} onPress={()=>setDropdown(!dropdown)}>
-            <View style={styles.picker}><Text style={styles.pickertext}>Select month  {dropdown ? `-` : `+`}</Text></View>
-        </TouchableHighlight>
-        {dropdown && 
-        <View style={styles.dropdown}>
-        {
-            Months.map((m,i)=>{
-                let ii = i+1;
-                let test = ''
-                    if(month === ii.toString()){
-                
-                    }
-                
-                return <TouchableOpacity key={i} onPress={()=>{setMonth(ii.toString()); setDropdown(false);}}><View style={styles.datepicker}><Text style={styles.pickertext}>{(month === ii.toString()) && "\u2713"}  {m}</Text></View></TouchableOpacity>
-            })
-        }
-        </View>
-        }
-        </>
-    )
-}
+  month: string;
+  setMonth: (value: string) => void;
+};
 
-const styles = StyleSheet.create({
-    dropdown:{
-        backgroundColor:"#eaeaea",
-        borderRadius:5,
-        marginTop:2
-    },
-    picker:{
-        padding:10,
-        color:'#000000',
-        borderRadius:5,
-        backgroundColor:'#bababa',
-      
-    },
-    col:{
-        color:"#0000FF"
-    },
-    pickertext:{
-        color:'#000000',
-        textAlign:"center",
-        fontWeight:700
-    },
-    datepicker:{
-        padding:5,
-        cursor:"pointer"
-    }
-})
+const MonthPicker = ({ month, setMonth }: PickerType) => {
+  const [dropdown, setDropdown] = useState(false);
+
+  return (
+    <>
+      <TouchableHighlight
+        style={{ borderRadius: 5 }}
+        onPress={() => setDropdown(!dropdown)}
+      >
+        <View style={styles.picker}>
+          <Text style={styles.pickertext}>
+            Select month {dropdown ? `-` : `+`}
+          </Text>
+        </View>
+      </TouchableHighlight>
+
+      {dropdown && (
+        <View style={styles.dropdown}>
+          {Months.map((m, i) => {
+            let ii = i + 1;
+            return (
+              <TouchableOpacity
+                key={i}
+                onPress={() => {
+                  setMonth(ii.toString());
+                  setDropdown(false);
+                }}
+              >
+                <View style={styles.datepicker}>
+                  <Text style={styles.pickertext}>
+                    {month === ii.toString() ? "\u2713 " : "  "} {m}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      )}
+    </>
+  );
+};
 
 export default MonthPicker;
+
+const styles = StyleSheet.create({
+  dropdown: {
+    backgroundColor: "#eaeaea",
+    borderRadius: 5,
+    marginTop: 2,
+  },
+  picker: {
+    padding: 10,
+    color: "#000000",
+    borderRadius: 5,
+    backgroundColor: "#bababa",
+  },
+  col: {
+    color: "#0000FF",
+  },
+  pickertext: {
+    color: "#000000",
+    textAlign: "center",
+    fontWeight: 700,
+  },
+  datepicker: {
+    padding: 5,
+    cursor: "pointer",
+  },
+});
